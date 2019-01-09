b38169e6be60935ace4f4010fb24ea399c8300eb
Alex Garcia and Ralph Perdomo <ralph@>
2019-01-08 16:25:32 -0800

    save

--- src/sudoku.js
+++ src/sudoku.js
@@ -29,15 +29,19 @@ export function horizParser(array){
   for (let outer =0; outer <3; outer++){
 
 
-    for(let inner =0; inner<3; inner++){
+    // for(let inner =0; inner<3; inner++){
+    //
+    //   // board[horiz[0][2]][horiz[0][0]]
+    //
+    //   board [horiz[0][0]] [horiz[0][1]]
+    //
+    //   // console.log(array[outer] horiz[inner]);
+    //
+    //     // array[0][0]
 
-      console.log(array[horiz[outer]][horiz[inner]);
+      results.push( board.s )
 
-        // array[0][0]
-
-      // results.push(array[horiz[outer][inner]])
-
-    }
+    // }
   }
 
   return results;
@@ -48,13 +52,43 @@ export function horizParser(array){
 
 
 var board = [
+  [1,2,3,4,5,6,7,8,9
+
+
+  ],
+  [1,2,3,4,5,6,7,8,9],
+  [1,2,33,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
+  [1,200,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9]
+  [1,2,3,4,5,6,7,42,9]
 ];
+// make a row
+// take 9 at a time
+// make a column
+// take every 9th
+// make a cell
+// take 3 skip 6
+
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81];
+
+function makeRows(b) {
+  let results = []
+
+  for (let i = 0; i<9; i++) {
+    results.push(board.splice(0,9));
+  }
+
+  return results;
+}
+
+function makeColumns(b) {
+  let results = []
+
+  for (let i = 0; i<9; i++) {
+    let indexPos = i*9
+    results.push(board[indexPos])
+  }
+}
